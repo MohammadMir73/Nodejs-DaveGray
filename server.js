2e365266a09abe27f3c00a0ef9f36e7662200845
Mohammad Mir <mirabdollahi.sm@gmail.com>
2023-01-21 20:23:13 +0330

    06TUT

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
-
+const app = express();
 const path = require('path');
 const PORT = process.env.port || 3500;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(200, '/new-page.html');
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attemped to load hello.html');
+    next();
+}, (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
